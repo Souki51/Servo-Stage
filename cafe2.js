432aecf250636bc0bf45abc272e0b9dd67620fc8
Souki51 <115420824+Souki51@users.noreply.github.com>
2022-12-04 21:32:27 +0100

    Add files via upload

---
+++ cafe2.js
@@ -0,0 +1,29 @@
+var cafe={
+"type": "FeatureCollection",
+"name": "cafe2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "osm_id": 444382336, "timestamp": null, "name": "Oukaimeden", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8478566, 33.996395 ] } },
+{ "type": "Feature", "properties": { "osm_id": 954684001, "timestamp": null, "name": "Nefertiti", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8309897, 34.0244046 ] } },
+{ "type": "Feature", "properties": { "osm_id": 1655665447, "timestamp": null, "name": "Paul", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8680169, 33.9598787 ] } },
+{ "type": "Feature", "properties": { "osm_id": 1655951266, "timestamp": null, "name": "Paul", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8484591, 33.9895242 ] } },
+{ "type": "Feature", "properties": { "osm_id": 1824603868, "timestamp": null, "name": null, "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8577141, 33.981477 ] } },
+{ "type": "Feature", "properties": { "osm_id": -1864776081, "timestamp": null, "name": "Schmid", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8457959, 33.9970852 ] } },
+{ "type": "Feature", "properties": { "osm_id": -1709239189, "timestamp": null, "name": "Galette de neige", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8251726, 34.0217225 ] } },
+{ "type": "Feature", "properties": { "osm_id": -1507701549, "timestamp": null, "name": null, "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8700429, 33.9869485 ] } },
+{ "type": "Feature", "properties": { "osm_id": -1460489123, "timestamp": null, "name": "Nejma", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8851102, 33.9610729 ] } },
+{ "type": "Feature", "properties": { "osm_id": -1445078137, "timestamp": null, "name": "O'Caprices", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8682658, 33.9601813 ] } },
+{ "type": "Feature", "properties": { "osm_id": -1310668891, "timestamp": null, "name": "Fennich", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8258274, 33.9901354 ] } },
+{ "type": "Feature", "properties": { "osm_id": -1104952346, "timestamp": null, "name": "Café Al Altal", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8263129, 34.0197676 ] } },
+{ "type": "Feature", "properties": { "osm_id": -928080910, "timestamp": null, "name": "Villa Jnane", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8395134, 34.0150348 ] } },
+{ "type": "Feature", "properties": { "osm_id": -885678707, "timestamp": null, "name": null, "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8302174, 34.0185206 ] } },
+{ "type": "Feature", "properties": { "osm_id": -884551754, "timestamp": null, "name": "Le Lingot D'or", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8367366, 34.0209855 ] } },
+{ "type": "Feature", "properties": { "osm_id": -877467739, "timestamp": null, "name": "La Sala", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8277524, 34.0163328 ] } },
+{ "type": "Feature", "properties": { "osm_id": -877425102, "timestamp": null, "name": "Il Buon Gelato", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8457693, 34.001741 ] } },
+{ "type": "Feature", "properties": { "osm_id": -858234061, "timestamp": null, "name": "Starbucks", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8677814, 33.9566821 ] } },
+{ "type": "Feature", "properties": { "osm_id": -589154947, "timestamp": null, "name": "Café Concorde", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8425154, 34.0202724 ] } },
+{ "type": "Feature", "properties": { "osm_id": -589154906, "timestamp": null, "name": "Casarina", "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8483331, 34.0041055 ] } },
+{ "type": "Feature", "properties": { "osm_id": -589133968, "timestamp": null, "name": null, "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8519836, 34.0021409 ] } },
+{ "type": "Feature", "properties": { "osm_id": -393385364, "timestamp": null, "name": null, "type": "cafe" }, "geometry": { "type": "Point", "coordinates": [ -6.8499117, 34.0172698 ] } }
+]
+}
---
+++ js/scripts.js
@@ -0,0 +1,85 @@
+/*!
+* Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+ 
+    
+    
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+// slide 
+    
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("custom-slider");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
